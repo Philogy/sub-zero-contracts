c3d9952338fa589325eaa263c4b26d5dd1eabae1
Philippe Dumonet <philippe@dumo.net>
2024-03-30 16:17:10 +0000

    ✨ Make deployment create3+nonce based

--- src/TradableAddresses.sol
+++ src/TradableAddresses.sol
@@ -7,10 +7,10 @@ import {PermitERC721} from "./base/PermitERC721.sol";
 import {IRenderer} from "./interfaces/IRenderer.sol";
 // Libraries.
 import {SafeTransferLib} from "solady/src/utils/SafeTransferLib.sol";
-import {SignatureCheckerLib} from "solady/src/utils/SignatureCheckerLib.sol";
 import {Create2Lib} from "./utils/Create2Lib.sol";
 import {BytesLib} from "./utils/BytesLib.sol";
 import {SaltLib} from "./utils/SaltLib.sol";
+import {LibRLP} from "solady/src/utils/LibRLP.sol";
 
 /// @author philogy <https://github.com/philogy>
 contract TradableAddresses is Ownable, PermitERC721 {
@@ -18,10 +18,8 @@ contract TradableAddresses is Ownable, PermitERC721 {
     using SaltLib for uint256;
     using SafeTransferLib for address;
 
+    error NotAuhtorizedBuyer();
     error InvalidFee();
-
-    error PastDeadline();
-    error InvalidSignature();
 
     error NoSource();
     error InvalidSource();
@@ -34,17 +32,25 @@ contract TradableAddresses is Ownable, PermitERC721 {
     event RendererSet(address indexed renderer);
     event FeeSet(uint16 fee);
 
-    uint96 internal constant NOT_MINTED = 0;
-    uint96 internal constant ALREADY_MINTED = 1;
+    uint96 internal constant MINTED_BIT = 0x100;
     uint256 internal constant BPS = 10000;
 
-    bytes32 internal immutable MINT_AND_SELL_TYPEHASH =
-        keccak256("MintAndSell(uint256 salt,uint256 amount,address beneficiary,uint256 nonce,uint256 deadline)");
+    // TODO: Mine and insert nonce incrementer.
+    bytes internal DEPLOY_PROXY_INITCODE;
+    bytes32 internal immutable DEPLOY_PROXY_INITHASH;
+
+    bytes32 internal immutable MINT_AND_SELL_TYPEHASH = keccak256(
+        "MintAndSell(uint256 salt,uint8 saltNonce,uint256 amount,address beneficiary,address buyer,uint256 nonce,uint256 deadline)"
+    );
 
     address public renderer;
     uint16 public buyFeeBps;
 
-    constructor(address initialOwner) {
+    constructor(address initialOwner, address nonceIncreaser) {
+        DEPLOY_PROXY_INITCODE = abi.encodePacked(
+            hex"602e8060095f395ff360013d363d3d373d823d73", nonceIncreaser, hex"5af4503603600134f03d5260203df3"
+        );
+        DEPLOY_PROXY_INITHASH = keccak256(DEPLOY_PROXY_INITCODE);
         _initializeOwner(initialOwner);
     }
 
@@ -77,25 +83,30 @@ contract TradableAddresses is Ownable, PermitERC721 {
     function mintAndBuy(
         address to,
         uint256 salt,
+        uint8 saltNonce,
         address beneficiary,
         uint256 sellerAmount,
+        address buyer,
         uint256 nonce,
         uint256 deadline,
         bytes calldata signature
     ) external payable {
-        // Validate sale.
-        if (block.timestamp > deadline) revert PastDeadline();
-        bytes32 hash = _hashTypedData(
-            keccak256(abi.encode(MINT_AND_SELL_TYPEHASH, salt, sellerAmount, beneficiary, nonce, deadline))
-        );
+        _checkDeadline(deadline);
         address owner = salt.owner();
-        if (!SignatureCheckerLib.isValidSignatureNowCalldata(owner, hash, signature)) revert InvalidSignature();
-        _useNonce(owner, nonce);
-        _mint(to, salt);
-        // Distribute ETH (fee is kept as contract balance).
-        uint256 feeRate = buyFeeBps;
-        uint256 fee = sellerAmount * feeRate / (BPS - feeRate);
-        uint256 amountLeft = msg.value - fee - sellerAmount;
+        _checkAndUseNonce(owner, nonce);
+        _checkBuyer(buyer);
+        bytes32 hash = _hashTypedData(
+            keccak256(
+                abi.encode(MINT_AND_SELL_TYPEHASH, salt, saltNonce, sellerAmount, beneficiary, buyer, nonce, deadline)
+            )
+        );
+        _checkSignature(owner, hash, signature);
+
+        _mint(to, salt, saltNonce);
+
+        uint256 buyCost = sellerAmount * BPS / (BPS - buyFeeBps);
+        // Checked subtraction will underflow if insufficient funds were sent.
+        uint256 amountLeft = msg.value - buyCost;
         beneficiary.safeTransferETH(sellerAmount);
         if (amountLeft > 0) msg.sender.safeTransferETH(amountLeft);
     }
@@ -104,45 +115,77 @@ contract TradableAddresses is Ownable, PermitERC721 {
     //                          MINTING                           //
     ////////////////////////////////////////////////////////////////
 
-    function mint(address to, uint256 salt) public {
-        if (msg.sender != address(this)) {
-            address owner = salt.owner();
-            if (msg.sender != owner && !isApprovedForAll(owner, msg.sender)) revert NotOwnerOrOperator();
-        }
-        _mint(to, salt);
+    function mint(address to, uint256 salt, uint8 nonce) public {
+        address owner = salt.owner();
+        if (msg.sender != owner && !isApprovedForAll(owner, msg.sender)) revert NotOwnerOrOperator();
+        _mint(to, salt, nonce);
     }
 
-    function mintMany(address to, uint256[] calldata newSalts) public {
-        uint256 saltsLength = newSalts.length;
+    struct NewSalt {
+        uint256 salt;
+        uint8 nonce;
+    }
+
+    function mintMany(address to, NewSalt[] calldata salts) public {
+        uint256 totalSalts = salts.length;
         unchecked {
-            for (uint256 i = 0; i < saltsLength; i++) {
-                mint(to, newSalts[i]);
+            for (uint256 i = 0; i < totalSalts; i++) {
+                NewSalt calldata newSalt = salts[i];
+                mint(to, newSalt.salt, newSalt.nonce);
             }
         }
     }
 
-    function _mint(address to, uint256 salt) internal override {
-        if (alreadyMinted(salt)) revert AlreadyMinted();
-        _mintAndSetExtraDataUnchecked(to, salt, ALREADY_MINTED);
+    function _mint(address to, uint256 salt, uint8 nonce) internal {
+        (bool minted,) = getTokenData(salt);
+        if (minted) revert AlreadyMinted();
+        _mintAndSetExtraDataUnchecked(to, salt, _packMinted(nonce));
     }
 
     ////////////////////////////////////////////////////////////////
-    //                   DEPLOYMENT & SOURCING                    //
+    //                         DEPLOYMENT                         //
     ////////////////////////////////////////////////////////////////
 
-    function deploy(uint256 salt, address source, bytes calldata payload) public payable returns (address deployed) {}
+    function deploy(uint256 id, bytes calldata initcode) public payable returns (address deployed) {
+        if (!approvedOrOwner(msg.sender, id)) revert NotOwnerNorApproved();
+        (, uint8 nonce) = getTokenData(id);
+        bytes memory deployProxyInitcode = DEPLOY_PROXY_INITCODE;
+        assembly ("memory-safe") {
+            let deployProxy := create2(callvalue(), add(deployProxyInitcode, 0x20), mload(deployProxyInitcode), id)
+            let m := mload(0x40)
+            mstore8(m, nonce)
+            calldatacopy(add(m, 1), initcode.offset, initcode.length)
+        }
+    }
 
     ////////////////////////////////////////////////////////////////
     //                          HELPERS                           //
     ////////////////////////////////////////////////////////////////
 
-    function alreadyMinted(uint256 id) public view returns (bool) {
-        return _getExtraData(id) == ALREADY_MINTED;
-    }
-
     function approvedOrOwner(address operator, uint256 id) public view returns (bool) {
         address owner = ownerOf(id);
-        return operator == owner || isApprovedForAll(owner, operator);
+        return operator == owner || isApprovedForAll(owner, operator) || getApproved(id) == operator;
+    }
+
+    function getTokenData(uint256 id) public view returns (bool minted, uint8 nonce) {
+        uint96 extraData = _getExtraData(id);
+        minted = extraData & MINTED_BIT != 0;
+        nonce = uint8(extraData);
+    }
+
+    function addressOf(uint256 id) public view returns (address vanity) {
+        (bool minted, uint8 nonce) = getTokenData(id);
+        if (!minted) revert TokenDoesNotExist();
+        vanity = computeAddress(bytes32(id), nonce);
+    }
+
+    function computeAddress(bytes32 salt, uint8 nonce) public view returns (address vanity) {
+        address deployProxy = Create2Lib.predict(DEPLOY_PROXY_INITHASH, salt, address(this));
+        vanity = LibRLP.computeAddress(deployProxy, nonce);
+    }
+
+    function _packMinted(uint8 nonce) internal pure returns (uint96) {
+        return uint96(nonce) | MINTED_BIT;
     }
 
     ////////////////////////////////////////////////////////////////
@@ -160,7 +203,15 @@ contract TradableAddresses is Ownable, PermitERC721 {
     function tokenURI(uint256 id) public view override returns (string memory) {
         address currentRenderer = renderer;
         if (currentRenderer == address(0)) revert NoRenderer();
-        address vanityAddr = Create2Lib.predict({initCodeHash: bytes32(0), salt: bytes32(id), deployer: address(this)});
+        address vanityAddr = addressOf(id);
         return IRenderer(currentRenderer).render(id, vanityAddr);
+    }
+
+    function _checkBuyer(address buyer) internal view {
+        bool authorized;
+        assembly ("memory-safe") {
+            authorized := or(iszero(buyer), eq(buyer, caller()))
+        }
+        if (!authorized) revert NotAuhtorizedBuyer();
     }
 }
--- src/base/PermitERC721.sol
+++ src/base/PermitERC721.sol
@@ -4,20 +4,46 @@ pragma solidity ^0.8.4;
 import {ERC721} from "solady/src/tokens/ERC721.sol";
 import {EIP712} from "solady/src/utils/EIP712.sol";
 import {LibBitmap} from "solady/src/utils/LibBitmap.sol";
+import {SignatureCheckerLib} from "solady/src/utils/SignatureCheckerLib.sol";
 
 /// @author philogy <https://github.com/philogy>
 abstract contract PermitERC721 is ERC721, EIP712 {
     using LibBitmap for LibBitmap.Bitmap;
 
     error NonceAlreadyUsed();
+    error PastDeadline();
+    error InvalidSignature();
+
+    /// @dev Copied from Solady as it's private.
+    uint256 private constant _ERC721_MASTER_SLOT_SEED_MASKED = 0x0a5a2e7a00000000;
+    /// @dev `keccak256(bytes("ApprovalForAll(address,address,bool)"))`.
+    uint256 private constant _APPROVAL_FOR_ALL_EVENT_SIGNATURE =
+        0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31;
+
+    bytes32 internal immutable PERMIT_FOR_ALL_TYPEHASH =
+        keccak256("PermitForAll(address owner,address operator,uint256 nonce,uint256 deadline)");
 
     mapping(address => LibBitmap.Bitmap) internal _nonces;
 
-    function nonceSet(address user, uint256 nonce) external view returns (bool) {
+    function permitForAll(address owner, address operator, uint256 nonce, uint256 deadline, bytes calldata signature)
+        external
+    {
+        _checkDeadline(deadline);
+        _checkAndUseNonce(owner, nonce);
+        bytes32 hash = _hashTypedData(keccak256(abi.encode(PERMIT_FOR_ALL_TYPEHASH, owner, operator, nonce, deadline)));
+        _checkSignature(owner, hash, signature);
+        _setIsApprovedForAll(owner, operator, true);
+    }
+
+    function invalidateNonce(uint256 nonce) external {
+        _checkAndUseNonce(msg.sender, nonce);
+    }
+
+    function getNonceIsSet(address user, uint256 nonce) external view returns (bool) {
         return _nonces[user].get(nonce);
     }
 
-    function _useNonce(address user, uint256 nonce) internal {
+    function _checkAndUseNonce(address user, uint256 nonce) internal {
         if (!_nonces[user].toggle(nonce)) revert NonceAlreadyUsed();
     }
 
@@ -27,5 +53,29 @@ abstract contract PermitERC721 is ERC721, EIP712 {
 
     function _version() internal view virtual returns (string memory) {
         return "1.0";
+    }
+
+    function _checkDeadline(uint256 deadline) internal view {
+        if (block.timestamp > deadline) revert PastDeadline();
+    }
+
+    function _checkSignature(address signer, bytes32 hash, bytes calldata signature) internal view {
+        if (!SignatureCheckerLib.isValidSignatureNowCalldata(signer, hash, signature)) revert InvalidSignature();
+    }
+
+    /// @dev Reference: Solady's `ERC721.setApprovalForAll`.
+    function _setIsApprovedForAll(address owner, address operator, bool approved) internal {
+        // Set `isApprovedForAll(owner, operator) == approved`.
+        assembly ("memory-safe") {
+            // Update the `isApproved` for (`msg.sender`, `operator`).
+            mstore(0x1c, operator)
+            mstore(0x08, _ERC721_MASTER_SLOT_SEED_MASKED)
+            mstore(0x00, owner)
+            sstore(keccak256(0x0c, 0x30), iszero(iszero(approved)))
+            // Emit the {ApprovalForAll} event.
+            mstore(0x00, true)
+            // forgefmt: disable-next-item
+            log3(0x00, 0x20, _APPROVAL_FOR_ALL_EVENT_SIGNATURE, caller(), shr(96, shl(96, operator)))
+        }
     }
 }
