84b678e36f5340b58dca96c83a2db178fc441e72
Philippe Dumonet <philippe@dumo.net>
2024-04-01 09:44:12 +0100

    🔨 Add tests

--- test/TradableAddresses.t.sol
+++ test/TradableAddresses.t.sol
@@ -7,6 +7,8 @@ import {TradableAddresses} from "../src/TradableAddresses.sol";
 import {LibRLP} from "solady/src/utils/LibRLP.sol";
 import {Create2Lib} from "../src/utils/Create2Lib.sol";
 import {MockSimple} from "./mocks/MockSimple.sol";
+import {FailingDeploy} from "./mocks/FailingDeploy.sol";
+import {Empty} from "./mocks/Empty.sol";
 
 import {console2 as console} from "forge-std/console2.sol";
 
@@ -38,5 +40,35 @@ contract TradableAddressesTest is Test, HuffTest {
         assertEq(address(simp).code, type(MockSimple).runtimeCode);
         assertEq(simp.balanceOf(user), 10e18);
         assertEq(simp.balanceOf(makeAddr("other")), 0);
+    }
+
+    function test_bubblesDeployRevert() public {
+        address user = makeAddr("user");
+        bytes32 salt = bytes32((uint256(uint160(user)) << 96) | 0xab19c31);
+        uint256 id = uint256(salt);
+        uint8 nonce = 21;
+        vm.prank(user);
+        trader.mint(user, salt, nonce);
+        assertEq(trader.ownerOf(id), user);
+
+        vm.prank(user);
+        vm.expectRevert(TradableAddresses.DeploymentFailed.selector);
+        trader.deploy(id, type(FailingDeploy).creationCode);
+    }
+
+    function test_bubblesIncreaseRevert() public {
+        address user = makeAddr("user");
+        bytes32 salt = bytes32((uint256(uint160(user)) << 96) | 0xab19c31);
+        uint256 id = uint256(salt);
+        uint8 nonce = 255;
+        vm.prank(user);
+        trader.mint(user, salt, nonce);
+        assertEq(trader.ownerOf(id), user);
+
+        vm.prank(user);
+        vm.expectRevert(TradableAddresses.DeploymentFailed.selector);
+        // Ensure out-of-gas within increaser but sufficient remaining gas to test whether
+        // standalone increase revert will actually get bubbled up.
+        trader.deploy{gas: 250 * 32000}(id, type(Empty).creationCode);
     }
 }
---
+++ test/mocks/Empty.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @author philogy <https://github.com/philogy>
+contract Empty {}
