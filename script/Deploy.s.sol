110f5e9df52e1d7a4dd74e8988a01211c45eee1c
Philippe Dumonet <philippe@dumo.net>
2024-06-30 11:36:25 +0100

    👷 Tweak script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -13,10 +13,15 @@ interface ICreateX {
 
 /// @author philogy <https://github.com/philogy>
 contract DeployScript is Test, Script, HuffTest {
-    ICreateX internal constant CREATEX = ICreateX(0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed);
+    ICreateX constant CREATEX = ICreateX(0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed);
+
+    address constant MARKET_OWNER = 0xea57c1ef7eF1c88b456ADf0927ec0EAe3B17f1F5;
+    bytes32 constant MARKET_SALT = 0x000000000000000000000000000000000000000094038e9be572f900652a472c;
 
     function run() public {
         vm.startBroadcast();
+
+        require(address(CREATEX).code.length > 0, "No CreateX deployed to your chain");
 
         address microCreateOut =
             CREATEX.deployCreate2(bytes32(0), hex"7160203d3581360380833d373d34f53d523df33d526012600ef3");
@@ -30,13 +35,10 @@ contract DeployScript is Test, Script, HuffTest {
         address increaser = abi.decode(ret, (address));
         require(increaser == NONCE_INCREASER, "Constructor failed");
 
-        address owner = vm.envAddress("MARKET_OWNER");
-        bytes memory bytecode = abi.encodePacked(type(VanityMarket).creationCode, abi.encode(owner));
+        bytes memory bytecode = abi.encodePacked(type(VanityMarket).creationCode, abi.encode(MARKET_OWNER));
         console.log("initcode hash: %x", uint256(keccak256(bytecode)));
 
-        bytes32 salt = vm.envBytes32("MARKET_SALT");
-
-        (success, ret) = MICRO_CREATE2.call(abi.encodePacked(salt, bytecode));
+        (success, ret) = MICRO_CREATE2.call(abi.encodePacked(MARKET_SALT, bytecode));
         if (!success || ret.length < 0x20) revert("Creation failed");
         address marketAddr = abi.decode(ret, (address));
         if (marketAddr == address(0)) revert("Constructor failed");
