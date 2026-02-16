b8adcaeb77506f64045edb2fca8c53222865263e
Tino Martínez Molina <tino.molina@layerzerolabs.org>
2026-02-16 10:36:09 -0800

    refactor: `HuffTest` declared addresses to `Addresses`

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
-import {Test} from "forge-std/Test.sol";
 import {console2 as console} from "forge-std/console2.sol";
-import {HuffTest} from "../test/base/HuffTest.sol";
 import {CreationCodes} from "./utils/CreationCodes.sol";
+import {Addresses} from "src/utils/Addresses.sol";
 
 interface ICreateX {
     function deployCreate2(bytes32 salt, bytes calldata initCode) external payable returns (address);
@@ -15,7 +14,7 @@ interface ICreateX {
  * @notice Main script entrypoint. Deploys contracts with expected vanity addresses to the chain.
  * @author philogy <https://github.com/philogy>
  */
-contract DeployScript is Test, Script, HuffTest, CreationCodes {
+contract DeployScript is Script, Addresses, CreationCodes {
     ICreateX constant CREATEX = ICreateX(0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed);
 
     address constant MARKET_OWNER = 0xea57c1ef7eF1c88b456ADf0927ec0EAe3B17f1F5;
@@ -66,11 +65,11 @@ contract DeployScript is Test, Script, HuffTest, CreationCodes {
 
         require(address(CREATEX).code.length > 0, "No CreateX deployed to your chain");
 
-        deployMicroCreate2({_salt: bytes32(0), _expectedAddress: MICRO_CREATE2});
+        deployMicroCreate2({_salt: bytes32(0), _expectedAddress: MICRO_CREATE2_ADDRESS});
 
-        deployNonceIncreaser({_microCreate2: MICRO_CREATE2, _expectedAddress: NONCE_INCREASER});
+        deployNonceIncreaser({_microCreate2: MICRO_CREATE2_ADDRESS, _expectedAddress: NONCE_INCREASER_ADDRESS});
 
-        deployVanityMarket({_microCreate2: MICRO_CREATE2, _expectedAddress: VANITY_MARKET});
+        deployVanityMarket({_microCreate2: MICRO_CREATE2_ADDRESS, _expectedAddress: VANITY_MARKET_ADDRESS});
 
         vm.stopBroadcast();
     }
@@ -83,20 +82,20 @@ contract DeployScript is Test, Script, HuffTest, CreationCodes {
 contract DeployProbeScript is DeployScript {
     bytes32 internal constant MICRO_CREATE2_PROBE_SALT = keccak256("safety first!");
 
-    address internal constant MICRO_CREATE2_PROBE = 0xb2EbA5Ac9d47E1F57639A115dBf7148Fab863A7E;
-    address internal constant NONCE_INCREASER_PROBE = 0x275211f908553595A0B5fA9dE5E6B1dbac52B5e2;
-    address internal constant VANITY_MARKET_PROBE = 0x3f3506b136D59CA6d0B75418559f7bA5B686f797;
+    address internal constant MICRO_CREATE2_PROBE_ADDRESS = 0xb2EbA5Ac9d47E1F57639A115dBf7148Fab863A7E;
+    address internal constant NONCE_INCREASER_PROBE_ADDRESS = 0x275211f908553595A0B5fA9dE5E6B1dbac52B5e2;
+    address internal constant VANITY_MARKET_PROBE_ADDRESS = 0x3f3506b136D59CA6d0B75418559f7bA5B686f797;
 
     function run() public virtual override {
         vm.startBroadcast();
 
         require(address(CREATEX).code.length > 0, "No CreateX deployed to your chain");
 
-        deployMicroCreate2({_salt: MICRO_CREATE2_PROBE_SALT, _expectedAddress: MICRO_CREATE2_PROBE});
+        deployMicroCreate2({_salt: MICRO_CREATE2_PROBE_SALT, _expectedAddress: MICRO_CREATE2_PROBE_ADDRESS});
 
-        deployNonceIncreaser({_microCreate2: MICRO_CREATE2_PROBE, _expectedAddress: NONCE_INCREASER_PROBE});
+        deployNonceIncreaser({_microCreate2: MICRO_CREATE2_PROBE_ADDRESS, _expectedAddress: NONCE_INCREASER_PROBE_ADDRESS});
 
-        deployVanityMarket({_microCreate2: MICRO_CREATE2_PROBE, _expectedAddress: VANITY_MARKET_PROBE});
+        deployVanityMarket({_microCreate2: MICRO_CREATE2_PROBE_ADDRESS, _expectedAddress: VANITY_MARKET_PROBE_ADDRESS});
 
         vm.stopBroadcast();
     }
---
+++ src/utils/Addresses.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+abstract contract Addresses {
+    address internal constant MICRO_CREATE2_ADDRESS = 0x6D9FB3C412a269Df566a5c92b85a8dc334F0A797;
+    address internal constant NONCE_INCREASER_ADDRESS = 0x00000000000001E4A82b33373DE1334E7d8F4879;
+    address internal constant VANITY_MARKET_ADDRESS = 0x000000000000b361194cfe6312EE3210d53C15AA;
+}
